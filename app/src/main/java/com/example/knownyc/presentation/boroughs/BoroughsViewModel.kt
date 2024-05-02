a65291326240c1765e3db1b67a50186bd1584a20
Bosch <boschio91@gmail.com>
2024-05-02 19:04:32 -0400

    Implemented boroughs view model

--- app/src/main/java/com/example/knownyc/presentation/boroughs/BoroughsViewModel.kt
+++ app/src/main/java/com/example/knownyc/presentation/boroughs/BoroughsViewModel.kt
@@ -1,27 +1,62 @@
 package com.example.knownyc.presentation.boroughs
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.knownyc.commons.Either
+import com.example.knownyc.commons.Event
+import com.example.knownyc.commons.TAG
+import com.example.knownyc.commons.sendEvent
+import com.example.knownyc.domain.repositories.BoroughsRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class BoroughsViewModel @Inject constructor(
-// we need a repository
+    private val repository: BoroughsRepository
 ) :ViewModel(){
     private val _state = MutableStateFlow(BoroughsUIState())
     val state = _state.asStateFlow()
 
     init {
+        Log.d(TAG, "ViewModel init: getting boroughs")
         getBoroughs()
     }
 
     private fun getBoroughs() {
         viewModelScope.launch {
             // load the data
+            _state.update {
+                it.copy(isLoading = true)
+            }
+
+            when (val result = repository.getBoroughs()) {
+                is Either.Data -> {
+                    Log.d(TAG, "Successfully loaded boroughs data")
+                    _state.update {
+                        it.copy(
+                            boroughs = result.value
+                        )
+                    }
+                }
+
+                is Either.Error -> {
+                    Log.e(TAG, "Error: loading boroughs data")
+                    _state.update {
+                        it.copy(
+                            error = result.error
+                        )
+                    }
+                    sendEvent(Event.Toast(result.error.message))
+                }
+            }
+            _state.update {
+                it.copy(isLoading = false)
+            }
         }
     }
 }
