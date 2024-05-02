efa255dfcb8f2f5e2fa93f5b2b0cb743299ba1d1
Bosch <boschio91@gmail.com>
2024-05-02 18:51:16 -0400

    Implemented exception handling, mappers, asset provider and borough repo

---
+++ app/src/main/java/com/example/knownyc/util/AppProviderModule.kt
@@ -0,0 +1,49 @@
+package com.example.knownyc.util
+
+import com.example.knownyc.data.local.provider.AssetsProvider
+import com.example.knownyc.data.local.provider.AssetsProviderImpl
+import com.example.knownyc.data.local.repository.BoroughsRepositoryImpl
+import com.example.knownyc.domain.repositories.BoroughsRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class AppProviderModule {
+
+    // Local Assets
+    @Binds
+    @Singleton
+    abstract fun localAssetsProvider(impl: AssetsProviderImpl): AssetsProvider
+
+    // Repositories
+    @Binds
+    @Singleton
+    abstract fun boroughsRepositoryProvider(impl: BoroughsRepositoryImpl): BoroughsRepository
+
+//    @Binds
+//    @Singleton
+//    abstract fun nycParksLocalRepositoryProvider(impl: com.example.knownyc.data.remote.NycParksRepositoryImpl): NycParksRepository
+//
+//    // API service
+//    companion object {
+//        @Singleton
+//        @Provides
+//        fun nycOpenDataApiServiceProvider(): NycOpenDataApiService {
+//            val json = Json {
+//                ignoreUnknownKeys = true
+//            }
+//
+//            Log.d(TAG, "building NYC Open Data API service provider")
+//
+//            return Retrofit.Builder().addConverterFactory(
+//                json.asConverterFactory("application.json".toMediaType())
+//            ).baseUrl(AppConstants.NYC_OPEN_DATA_API_BASE_URL).build()
+//                .create(NycOpenDataApiService::class.java)
+//        }
+//    }
+
+}
